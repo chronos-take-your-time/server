0e19ba40c8e79e9555f202da07d29bb8534363ea
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-26 17:37:17 -0300

    fix: correções nos nomes das rotas

--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const rootRoute = require('./routes/ola');
-const olaRoute = require('./routes/ola');
+const rootRouter = require('./routes/root');
+const olaRouter = require('./routes/ola');
 
-app.use('/', rootRoute);
-app.use('/ola', olaRoute);
+app.use('/', rootRouter);
+app.use('/ola', olaRouter);
 
 app.listen(port, () => {
   console.debug(`[SUCCESS]: Chronos listening on port ${port}`);
 });
-
-
