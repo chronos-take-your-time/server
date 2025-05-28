8641810b2ba149159cdf44f5e6fd025c8e8a1931
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-28 18:04:30 +0000

    add(route): boards routes

---
+++ src/routes/boards.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// update a existing board
+app.post('/:team_id/:id', (req, res) => {
+  
+});
+
+// get a board JSON
+app.get('/:team_id/:id', (req, res) => {
+  
+});
+
+// delete a board
+app.delete('/:team_id/:id', (req, res) => {
+});
+
+module.exports = router;
