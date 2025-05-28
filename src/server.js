2eb584faa726c168f860548c1790192798b25ed7
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-28 18:03:37 +0000

    add(routes): team and board routers

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 import { createClerkClient } from '@clerk/backend'
 
+// setup app
 const express = require('express');
 const app = express();
 const port = 3000;
 
 // routes
 const rootRouter = require('./routes/root');
-const olaRouter = require('./routes/ola');
+const teamRouter = require('./routes/teams');
+const boardRouter = require('./routes/boards');
 app.use('/', rootRouter);
-app.use('/ola', olaRouter);
+app.use('/teams', teamRouter);
+app.use('/boards', boardRouter);
 
 // clerk
 const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
 
-app.listen(port, () => {
-  console.debug(`[SUCCESS]: Chronos listening on port ${port}`);
-});
+app.listen(port, () => { console.debug(`[SUCCESS]: Chronos listening on port ${port}`) });
