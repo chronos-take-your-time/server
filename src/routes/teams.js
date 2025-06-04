ba4b928469e30c7a6664795994cf4b3212542b11
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-06-04 17:37:33 +0000

    fix: usando router ao inves de app

--- src/routes/teams.js
+++ src/routes/teams.js
@@ -3,33 +3,33 @@ const router = express.Router();
 const { handleResponse } = require('../utils/handleResponse');
 
 // create a new team
-app.post('/create/:user_id', (req, res) => {
+router.post('/create/:user_id', (req, res) => {
   // verify if user_id is valid
   // create a team with owner_id as user_id
 });
 
 // get members id of an existing team
-app.get('/:team_id/', (req, res) => {
+router.get('/:team_id/', (req, res) => {
   // verify if team_id is valid
   // return a list of members id
 });
 
 // add user by id to an existing team
-app.post('/:team_id/add/:user_id', (req, res) => {
+router.post('/:team_id/add/:user_id', (req, res) => {
   // verify if user_id is valid
   // verify if team_id is valid
   // add user to team
 });
 
 // remove user by id of an existing team
-app.post('/:team_id/remove/:user_id', (req, res) => {
+router.post('/:team_id/remove/:user_id', (req, res) => {
   // verify if user_id is valid
   // verify if team_id is valid
   // remove user to team
 });
 
 // delete a team
-app.delete('/:team_id', (req, res) => {
+router.delete('/:team_id', (req, res) => {
   // verify if is a valid team_id
   // delete the team and all files in its dir
 });
