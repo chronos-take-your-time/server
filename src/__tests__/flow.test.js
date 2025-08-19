88648a9f894f4bb788c9cf6aa958d769aaa8e843
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-08-19 17:32:28 +0000

    add: teste para a função getTeamBoards e limpeza

--- src/__tests__/flow.test.js
+++ src/__tests__/flow.test.js
@@ -1,22 +1,23 @@
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
-const { createTeam, deleteTeam } = require("../controllers/teams");
-const { createBoard, getBoard, deleteBoard } = require("../controllers/boards");
+const { getRandomBoardId } = require('../utils/random');
+const { createTeam, deleteTeam, getTeamBoards } = require('../controllers/teams');
+const { createBoard, getBoard, deleteBoard } = require('../controllers/boards');
 
 let tmpDir;
-const fakeTeamId = "org@test";
-const fakeBoardId = "chronos";
-const fakeBoardData = { ishow: "speed" };
+const fakeTeamId = 'org@test';
+const fakeBoardId = getRandomBoardId();
+const fakeBoardData = { ishow: 'speed' };
 
 // Helpers
-function setupBoard() {
-  createBoard(fakeTeamId, fakeBoardId, fakeBoardData, tmpDir);
+function setupBoard(boardId = fakeBoardId) {
+  createBoard(fakeTeamId, boardId, fakeBoardData, tmpDir);
 }
 
 beforeEach(() => {
-  tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "chronos-"));
+  tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'chronos-'));
   createTeam(fakeTeamId, tmpDir);
 });
 
@@ -25,40 +26,48 @@ afterEach(() => {
 });
 
 // --- TEAM TESTS ---
-describe("Team Controller", () => {
-  test("creates team successfully", () => {
-    const result = createTeam("newTeam", tmpDir);
-    expect(result.status).toBe("success");
+describe('Team Controller', () => {
+  test('creates team successfully', () => {
+    const result = createTeam('newTeam', tmpDir);
+    expect(result.status).toBe('success');
   });
 
-  test("deletes existing team successfully", () => {
+  test('deletes existing team successfully', () => {
     const result = deleteTeam(fakeTeamId, tmpDir);
-    expect(result.status).toBe("success");
+    expect(result.status).toBe('success');
   });
 
-  test("doesnt delete non-existing team", () => {
-    const result = deleteTeam("non_existing_team", tmpDir);
-    expect(result.status).toBe("error");
+  test('doesnt delete non-existing team', () => {
+    const result = deleteTeam('non_existing_team', tmpDir);
+    expect(result.status).toBe('error');
   });
 });
 
 // --- BOARD TESTS ---
-describe("Board Controller", () => {
-  test("creates board successfully", () => {
-    const result = createBoard(fakeTeamId, fakeBoardId, fakeBoardData, tmpDir);
-    expect(result.status).toBe("success");
+describe('Board Controller', () => {
+  test('creates board successfully', () => {
+    const result = setupBoard();
+    expect(result.status).toBe('success');
   });
 
-  test("returns board successfully", () => {
+  test('returns board successfully', () => {
     setupBoard();
     const result = getBoard(fakeTeamId, fakeBoardId, tmpDir);
-    expect(result.status).toBe("success");
+    expect(result.status).toBe('success');
     expect(result.data).toEqual(fakeBoardData);
   });
 
-  test("deletes board successfully", () => {
+  test('deletes board successfully', () => {
     setupBoard();
     const result = deleteBoard(fakeTeamId, fakeBoardId, tmpDir);
-    expect(result.status).toBe("success");
+    expect(result.status).toBe('success');
+  });
+
+  test('get team boards', () => {
+    const altBoardId = getRandomBoardId(); // test with another board just in case
+    setupBoard();
+    setupBoard(altBoardId);
+    const result = getTeamBoards(fakeTeamId, tmpDir);
+    expect(result).toStrictEqual([{boardId: fakeBoardId}, {boardId: altBoardId}])
   });
 });
