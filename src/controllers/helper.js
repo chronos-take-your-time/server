e5f3d0752059684bfe5e0b187a67fe8052fd88bf
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-08-20 13:52:03 +0000

    add: mensagem de erro em caso da falta do teamId

---
+++ src/controllers/helper.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const { humanOutput } = require("../utils/output");
+const baseDir = path.join(__dirname, "..", "teams");
+
+function getTeamPath(teamId, altDir = baseDir) {
+  if (altDir) {
+    altDir = baseDir;
+  }
+
+  if (!teamId) {
+    return humanOutput("error", "getTeamPath called without teamId");
+  }
+  return path.join(altDir, teamId);
+}
+
+module.exports = {
+  getTeamPath,
+  baseDir,
+};
