0bf5663b21bed781dc856f3035f37573bf3fb2cf
Pedro Henrique Alves <79018158+nukhes@users.noreply.github.com>
2025-10-01 14:09:26 -0300

    fix: configurando o CORS para requisições públicas

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 require('dotenv').config({ path: '../.env' });
 
-
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const port = 3000;
 
-// necessario para o parse dos jsons
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With, Accept, Accept, Host, Origin, Referer, Sec-Fetch-Dest, User-Agent, X-Forwarded-Host, X-Forwarded-Proto',
+    credentials: true,
+}));
+
+
 app.use(express.json());
 
-// routes
 const rootRouter = require('./routes/root');
 const teamRouter = require('./routes/teams');
 const boardRouter = require('./routes/boards');
+
 app.use('/', rootRouter);
 app.use('/teams', teamRouter);
 app.use('/boards', boardRouter);
