51965a23ac56f8ef5f39e2b7d239873e652a9c92
L-hso <lhsoliveira2008@hotmail.com>
2025-10-23 09:09:00 -0300

    feat: limite de tamanho do request aumentado para 50mb

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const { withAuth } = require('./utils/clerk');
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 app.use(cors({
     origin: '*',
     methods: 'GET,HEAD,PUT,POST,DELETE',
