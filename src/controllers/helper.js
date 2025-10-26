0e37c97551f2874992a2a62760d81875351f0aa8
L-hso <lhsoliveira2008@hotmail.com>
2025-10-26 14:09:23 -0300

    feat: método para obter o path do quadro

--- src/controllers/helper.js
+++ src/controllers/helper.js
@@ -1,15 +1,23 @@
 const path = require("path");
 const { humanOutput } = require("../utils/output");
+const path = require("path");
 const baseDir = path.join(__dirname, "..", "teams");
 
 function getTeamPath(teamId, altDir = baseDir) {
   if (!teamId) {
-    return humanOutput(400, 'getTeamPath called without teamId');
+    return humanOutput(400, "getTeamPath called without teamId");
   }
   return path.join(altDir, teamId);
 }
 
+function getBoardPath(teamId, boardId, altDir = baseDir) {
+  if (!teamId) return humanOutput(400, "getBoardPath called without teamId");
+  if (!boardId) return humanOutput(400, "getBoardPath called without boardId");
+  return path.join(altDir, teamId, `${boardId}.json`);
+}
+
 module.exports = {
   getTeamPath,
+  getBoardPath,
   baseDir,
 };
