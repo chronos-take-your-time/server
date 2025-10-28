d4a8b233a4a7c2c20c55b8852ecb9f5c304afd71
L-hso <lhsoliveira2008@hotmail.com>
2025-10-27 21:53:03 -0300

    feat: implementação parcial da atualização do conteúdo dos quadros

--- src/utils/tldraw.js
+++ src/utils/tldraw.js
@@ -4,22 +4,34 @@ const {
   defaultBindingSchemas,
   defaultShapeSchemas,
 } = require("@tldraw/tlschema");
+const { getBoard, updateBoardContent } = require("../controllers/boards");
 
 /**
  * Returns a new TLSocketRoom with default schemas and a timeout of 30s
  * @returns {TLSocketRoom}
  */
-function getRoom() {
+function getRoom(teamId, boardId) {
   const schema = createTLSchema({
     bindings: defaultBindingSchemas,
     shapes: defaultShapeSchemas,
   });
 
-  const room = new TLSocketRoom({ schema, clientTimeout: 30000 });
+  const board = getBoard(teamId, boardId);
+
+  const room = new TLSocketRoom({
+    schema,
+    clientTimeout: 30000,
+    initialSnapshot: board?.data?.content,
+    onDataChange: ()=>{
+      const currentSnapshot = room.getCurrentSnapshot();
+      //console.log(currentSnapshot)
+      updateBoardContent(teamId, boardId, currentSnapshot);
+    }
+  });
 
   return room;
 }
 
 module.exports = {
-  getRoom
-}
+  getRoom,
+};
